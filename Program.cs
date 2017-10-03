4a130689e409fea912a729fb6bae8070d7095cdc
Karina Lopez <karinalopez91@gmail.com>
2017-10-03 12:18:57 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Molly Guthrie", "Noodles", "Rockford, MI"));
+            students.Add(new Student("Michelle Jokish Polo","Spaghetti", "Ecuador"));
+            students.Add(new Student("Rebecca Allard", "Pizza", "Charlevoix, MI"));
+            students.Add(new Student("Brianna Jael", "Gumbo", "Fairfield,CA"));
+            students.Add(new Student("Tammy Cole", "Maru Sushi", "Grand Rapids,MI"));
+            students.Add(new Student("Lauren Babcock", "Cheeseburgers", "Plainwell,MI"));
+            students.Add(new Student("Karina Lopez", "Tacos", "Holland, MI"));
+            students.Add(new Student("Anel Guel-Juarez", "Pizza", "Grand Rapids,MI"));
+            students.Add(new Student("Lisa Dewey", "Mushroom Masala Dosa", "Los Angeles, CA"));
+            students.Add(new Student("Jonaca Hudson", "Sushi", "Lansing, MI"));
+            students.Add(new Student("Tanvi Sathe", "Chicken Biryani", "India"));
+            students.Add(new Student("Tommy Waalkes", "Indian Red Curries", "Raleigh, NC"));
+
+            bool loopAgain = true;
+            while (loopAgain == true)
+            {
+                try
+                {
+                    Console.WriteLine("Welcome to our C# class. Which student would you like to learn more about?Enter a number 1-12");
+                    int input = int.Parse(Console.ReadLine());
+                    Console.Write("Student " + input + " is ");
+                    // input-1 since the first entry in the list is 0, not 1. In a way it's re-numbering the students
+                    students[input - 1].Name();
+                    students[input - 1].FoodOrHometown();
+                }
+                //catches entries that are not numbers
+                catch (FormatException)
+                {
+                    Console.WriteLine("not a valid entry");
+                    loopAgain = true;
+                }
+                //catches indexes that are not in the list
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("That student does not exist");
+                    loopAgain = true;
+                }
+                
+                loopAgain = DoAgain();
+            }
+            
+
+        }
+        public static bool DoAgain()
+        {
+            string goAgain;
+            Console.WriteLine("Would you like to learn about another student?(Y or N)");
+            goAgain = Console.ReadLine();
+            goAgain = goAgain.ToLower();
+            bool repeat;
+            if (goAgain == "y")
+            {
+                repeat = true;
+            }
+            else if (goAgain == "n")
+            {
+                repeat = false;
+            }
+            else
+            {
+                Console.WriteLine("Sorry not an accurate input");
+                repeat = DoAgain();
+            }
+            return repeat;
+        }
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{   //creates student object
+    class Student
+    {
+        string name;
+        string food;
+        string homeTown;
+
+        public Student(string n, string f, string ht)
+        {
+            name = n;
+            food = f;
+            homeTown = ht;
+        }
+        public void Name()
+        {
+            Console.WriteLine(name);
+        }
+        public void PrintFood()
+        {
+            Console.WriteLine(name + "'s favorite food is " + food);
+        }
+        public void PrintHomeTown()
+        {
+            Console.WriteLine(name + "'s hometown is " + homeTown);
+        }
+        //method to give information about student's hometown or food and to tell user if invalid entry was entered
+        public  void FoodOrHometown()
+        {
+            Console.WriteLine("What would you like to know about "+name+ "(enter hometown or food)");
+            string i = Console.ReadLine();
+            i.ToLower();
+           
+            if (i == "food")
+            {
+                PrintFood();
+                
+            }
+            else if (i == "hometown")
+            {
+                PrintHomeTown();
+                
+            }
+            else
+            {
+                Console.WriteLine("That data does not exist. Please try again");
+               
+
+            }
+         
+        }
+
+    }   
+
+}
+
+    
+
+
